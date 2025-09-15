05e620a6c90093aa45389bb00762fe3858b97591
Johanna Nasr <if23b284@technikum-wien.at>
2025-09-15 19:24:38 +0200

    changes DocumentController

--- src/main/java/com/example/documentservice/controller/DocumentController.java
+++ src/main/java/com/example/documentservice/controller/DocumentController.java
@@ -1,9 +1,12 @@
 package com.example.documentservice.controller;
 
+import com.example.documentservice.entity.Document;
 import com.example.documentservice.service.DocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 public class DocumentController {
@@ -14,11 +17,27 @@ public class DocumentController {
     public String getDocumentbyId(@PathVariable("id") String id) {
         return this.documentService.findById(id).toString();
     }
-    @PostMapping("/document")
-    public String uploadDocument(@PathVariable MultipartFile file) {
-        return this.documentService.upload();
+
+    @GetMapping("/document")
+    public String getDocuments(@PathVariable("id") String id) {
+        return this.documentService.findAll().toString();
     }
 
+    @PostMapping("/document")
+    public String uploadDocument(@RequestBody Document document){/*@RequestParam("file") MultipartFile file,
+                                  @RequestParam(value = "title", required = false) String title,
+                                  @RequestParam(value = "created", required = false) String created,
+                                  @RequestParam(value = "correspondent", required = false) String correspondent,
+                                  @RequestParam(value = "document_type", required = false) String documentType,
+                                  @RequestParam(value = "storage_path", required = false) String storagePath,
+                                  @RequestParam(value = "tags", required = false) List<String> tags,
+                                  @RequestParam(value = "archive_serial_number", required = false) String archiveSerialNumber,
+                                  @RequestParam(value = "custom_fields", required = false) List<String> customFields)*/
+        return this.documentService.upload(document).toString();
+    }
 
-
+    /*@PutMapping("/document/{id}")
+    public String update(@PathVariable("id") String id){
+        return this.documentService.update(id).toString();
+    }*/
 }
