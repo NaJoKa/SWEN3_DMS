5f7f8e7c34a6b41910e02628a457db5ce74196e2
if23b269 <if23b269@technikum-wien.at>
2025-10-18 14:02:37 +0200

    add documents table
    Took 5 minutes

--- paperlessWebUI/webUI/src/app/components/documents/documents.ts
+++ paperlessWebUI/webUI/src/app/components/documents/documents.ts
@@ -1,15 +1,38 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Observable} from 'rxjs';
+import {DocumentService} from '../../document-service';
+import {AsyncPipe, DatePipe, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-documents',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    DatePipe
+  ],
   templateUrl: './documents.html',
   styleUrl: './documents.css'
 })
-export class Documents {
+export class Documents implements OnInit{
   @Output() settingsEvent = new EventEmitter<void>();
 
   emitSettingsEvent() {
     this.settingsEvent.emit();
+  }
+
+  documents$ = new Observable<any[]>;  // Observable for documents
+
+  constructor(private documentService: DocumentService) {}
+
+  ngOnInit(): void {
+    // Fetch documents as an Observable
+    this.documents$ = this.documentService.getDocuments();
+  }
+
+  // Navigate to the detail page when a document is clicked
+  viewDocument(id: string): void {
+    // In a real-world scenario, you might navigate to a different page with more details
+    // For example:
+    // this.router.navigate([`/document/${id}`]);
+    console.log(`View document with ID: ${id}`);
   }
 }
