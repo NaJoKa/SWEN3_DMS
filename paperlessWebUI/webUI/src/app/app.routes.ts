d6023ff4c97a508ab38bd039a2dbeeb10c3681db
Johanna Nasr <if23b284@technikum-wien.at>
2026-02-23 22:27:03 +0100

    add changes to webUI login and create register webUI

--- paperlessWebUI/webUI/src/app/app.routes.ts
+++ paperlessWebUI/webUI/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import {Documents} from './components/documents/documents';
 import {Dashboard} from './components/dashboard/dashboard';
 import {Upload} from './components/upload/upload';
 import {Login} from './components/login/login';
+import {Register} from './components/register/register';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: Dashboard },
   { path: 'documents', component: Documents },
   { path: 'upload', component: Upload },
-  { path: 'login', component: Login}
+  { path: 'login', component: Login},
+  { path: 'register', component: Register}
 ];
