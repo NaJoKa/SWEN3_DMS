4c2663db7e047ab94c7792461578ce9f5fd6874a
Johanna Nasr <if23b284@technikum-wien.at>
2026-02-23 22:09:15 +0100

    webUI login

--- paperlessWebUI/webUI/src/app/app.routes.ts
+++ paperlessWebUI/webUI/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {Documents} from './components/documents/documents';
 import {Dashboard} from './components/dashboard/dashboard';
 import {Upload} from './components/upload/upload';
+import {Login} from './components/login/login';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: Dashboard },
   { path: 'documents', component: Documents },
   { path: 'upload', component: Upload },
+  { path: 'login', component: Login}
 ];
