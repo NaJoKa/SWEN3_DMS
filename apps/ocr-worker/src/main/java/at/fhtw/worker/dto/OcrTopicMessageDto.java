4b166668a13e4eec9e230f351a08119841dd0511
Alexander <alexander@example.com>
2026-02-25 09:13:31 +0100

    ocr worker - change dtos, add lambok, add kafka topics to env variables

--- apps/ocr-worker/src/main/java/at/fhtw/worker/dto/OcrTopicMessageDto.java
+++ apps/ocr-worker/src/main/java/at/fhtw/worker/dto/OcrTopicMessageDto.java
@@ -1,34 +1,11 @@
 package at.fhtw.worker.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class OcrTopicMessageDto {
-    @JsonProperty("objectKey")
     private String objectKey;
-    @JsonProperty("bucket")
-    private String bucket;
-
-    public String getObjectKey() {
-        return objectKey;
-    }
-
-    public void setObjectKey(String objectKey) {
-        this.objectKey = objectKey;
-    }
-
-    public String getBucket() {
-        return bucket;
-    }
-
-    public void setBucket(String bucket) {
-        this.bucket = bucket;
-    }
-
-    @Override
-    public String toString() {
-        return "OcrTopicMessageDto{" +
-               "objectKey='" + objectKey + '\'' +
-               ", bucket='" + bucket + '\'' +
-               '}';
-    }
+    private String fileName;
 }
